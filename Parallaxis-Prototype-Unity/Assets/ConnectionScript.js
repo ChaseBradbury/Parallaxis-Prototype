fa223593b97585bf568199caf0d8b3b3291a9f80
Chase Bradbury <wbradbur@ucsc.edu>
2014-11-24 20:57:00 -0800

    A Complex Example
    Added constellations.

--- Parallaxis-Prototype-Unity/Assets/ConnectionScript.js
+++ Parallaxis-Prototype-Unity/Assets/ConnectionScript.js
@@ -13,9 +13,13 @@ function Start () {
 }
 
 function Update () {
-	var line: EdgeCollider2D = this.GetComponent(EdgeCollider2D);
+	var edge: EdgeCollider2D = this.GetComponent(EdgeCollider2D);
+	var line: LineRenderer = this.GetComponent(LineRenderer);
 	var point1: Vector2 = star1.position;
 	var point2: Vector2 = star2.position;
-	line.points = [point1, point2];
-	Debug.DrawLine(line.points[0], line.points[1], Color.white, 0.0f, true);
+	edge.points = [point1, point2];
+	line.SetPosition(0, point1);
+	line.SetPosition(1, point2);
+	
+	//Debug.DrawLine(line.points[0], line.points[1], Color.white, 0.0f, true);
 }
--- Parallaxis-Prototype-Unity/Assets/Mover.js
+++ Parallaxis-Prototype-Unity/Assets/Mover.js
@@ -8,13 +8,13 @@ function Start () {
 
 function Update () {
 	if(Input.GetKey("w")){
-		this.transform.position.y += 0.001 * layerPos;
+		this.transform.position.y += 0.01 * layerPos;
 	} else if(Input.GetKey("s")){
-		this.transform.position.y -= 0.001 * layerPos;
+		this.transform.position.y -= 0.01 * layerPos;
 	} else if(Input.GetKey("a")){
-		this.transform.position.x -= 0.001 * layerPos;
+		this.transform.position.x -= 0.01 * layerPos;
 	} else if(Input.GetKey("d")){
-		this.transform.position.x += 0.001 * layerPos;
+		this.transform.position.x += 0.01 * layerPos;
 	}
 	
 }
