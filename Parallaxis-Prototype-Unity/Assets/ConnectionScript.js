8d643f927c5c0bbc7d7356c21bb7527450d93ade
Chase Bradbury <chasebradbury@gmail.com>
2014-12-02 12:56:12 -0800

    ...And they're back...

---
+++ Parallaxis-Prototype-Unity/Assets/ConnectionScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var star1: Transform;
+var star2: Transform;
+
+
+function Start () {
+	var line: EdgeCollider2D = this.GetComponent(EdgeCollider2D);
+	var point1: Vector2 = star1.position;
+	var point2: Vector2 = star2.position;
+	line.points = [point1, point2];
+	//line.points[1] = star2.position;
+}
+
+function Update () {
+	var edge: EdgeCollider2D = this.GetComponent(EdgeCollider2D);
+	var line: LineRenderer = this.GetComponent(LineRenderer);
+	var point1: Vector2 = star1.position;
+	var point2: Vector2 = star2.position;
+	edge.points = [point1, point2];
+	line.SetPosition(0, point1);
+	line.SetPosition(1, point2);
+	
+	//Debug.DrawLine(line.points[0], line.points[1], Color.white, 0.0f, true);
+}
---
+++ Parallaxis-Prototype-Unity/Assets/Mover.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+
+var layerPos: int;
+var lastMouseCoordinate = Vector3.zero;
+
+function Start () {
+	lastMouseCoordinate.x = Screen.width/2;
+	lastMouseCoordinate.y = Screen.height/2;
+}
+
+function Update () {
+	var mouseDelta = Input.mousePosition - lastMouseCoordinate;
+	var xmag = Mathf.Abs(mouseDelta.x);
+	var ymag = Mathf.Abs(mouseDelta.y);
+	
+	
+	this.transform.position += layerPos * mouseDelta/1000;
+	/*
+	if(xmag > ymag) {
+	//mouse is moving mostly along the x axis
+	
+	if(mouseDelta.x > 0){
+		//RIGHT
+		this.transform.position.x += 0.01 * layerPos;
+	}
+	else {
+		//LEFT
+		this.transform.position.x -= 0.01 * layerPos;
+		}
+	}
+	else if (ymag > xmag) {
+	//mouse is moving mostly along y axis
+	
+		if (mouseDelta.y > 0){
+			//UP
+			this.transform.position.y += 0.01 * layerPos;
+			}
+		else {
+			//DOWN
+			this.transform.position.y -= 0.01 * layerPos;
+		}
+	}
+	else {
+		// NO MOVEMENT
+		// OR PERFECTLY SIDEWAYs
+		}	*/
+		
+	lastMouseCoordinate = Input.mousePosition;
+	
+	/*if(Input.GetKey("w")){
+		this.transform.position.y += 0.01 * layerPos;
+	} else if(Input.GetKey("s")){
+		this.transform.position.y -= 0.01 * layerPos;
+	} else if(Input.GetKey("a")){
+		this.transform.position.x -= 0.01 * layerPos;
+	} else if(Input.GetKey("d")){
+		this.transform.position.x += 0.01 * layerPos;
+	}
+	*/
+	
+}
