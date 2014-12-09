477bd7c5f7d02b727f3263fd9d18821bc5501925
Chase Bradbury <chasebradbury@gmail.com>
2014-12-08 16:08:49 -0800

    Added player sprites
    Added some temporary player sprites and a script to change the player
    sprite when it reaches the goal.

--- Parallaxis-Prototype-Unity/Assets/CharMovement.js
+++ Parallaxis-Prototype-Unity/Assets/CharMovement.js
@@ -24,3 +24,12 @@ function OnCollisionEnter2D(other : Collision2D){
     	canJump = true; //allow him to jump again
 	//}
 }
+
+function OnTriggerEnter2D (other : Collider2D) {
+	if (other.gameObject.tag == "Icon") {
+		this.GetComponent(SpriteRenderer).sprite = other.GetComponent(SpriteRenderer).sprite;
+		//transform.localScale = Vector3(0.32, 0.32, 0.32);
+		//this.GetComponent(BoxCollider2D).size = Vector3(2.56, 2.56, 2.56);
+		Destroy(other.gameObject);
+	}
+}
